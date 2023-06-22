49c43b6e9df3dc1b21c39f8e6d54e1a5f1d43db1
ansoncolam <lam22015@byui.edu>
2023-06-22 14:19:23 +0800

    updated

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -1,9 +1,18 @@
 using System;
 
-class Program
-{
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Learning04 World!");
+class Program {
+    static void Main(string[] args) {
+        
+        Assignment myAssignment = new Assignment("Samuel Bennett", "Multiplication");
+        Console.WriteLine(myAssignment.GetSummary());
+
+        MathAssignment myMathAssignment = new MathAssignment("Roberto Rodriguez", "Fractions", "7.3", "8-19");
+        Console.WriteLine(myMathAssignment.GetSummary());
+        Console.WriteLine(myMathAssignment.GetHomeworkList());
+
+        WritingAssignment myWritingAssignment = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
+        Console.WriteLine(myWritingAssignment.GetSummary());
+        Console.WriteLine(myWritingAssignment.GetWritingInformation());
+
     }
 }
