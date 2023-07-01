f9e1dfe2450ac59071d72299932d4376111f0eb2
ansoncolam <lam22015@byui.edu>
2023-07-01 10:07:28 +0800

    w05 learning activity

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+
+        Square mySquare = new Square("Red",2);
+        shapes.Add(mySquare);
+
+        Rectangle myRectangle = new Rectangle("Orange",2,3);
+        shapes.Add(myRectangle);
+
+        Circle myCircle = new Circle("Yellow",4);
+        shapes.Add(myCircle);
+
+        foreach (Shape shape in shapes) {
+            Console.WriteLine(shape.GetColor());
+            Console.WriteLine(shape.GetArea());
+        }
     }
 }
