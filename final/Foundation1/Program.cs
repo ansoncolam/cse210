7850a00017d249020beac8d5078cbaf2ab3dbea0
ansoncolam <lam22015@byui.edu>
2023-07-12 21:53:44 +0800

    updated foundation 1

--- final/Foundation1/Program.cs
+++ final/Foundation1/Program.cs
@@ -12,10 +12,21 @@ class Program
         Comment comment3 = new Comment("D_D12392", "LOL...");
         Comment comment4 = new Comment("FloraFung", "NEVER!!!");
 
+        videoList.Add(video1);
+        video1.SetComment(comment1);
+        video1.SetComment(comment2);
+        video1.SetComment(comment3);
+        video1.SetComment(comment4);
+
         Video video2 = new Video("What to expect in a Chinese Hotpot Restaurant", "Ellen133", 430);
         Comment comment5 = new Comment("grass_as_always", "@p@");
         Comment comment6 = new Comment("holyhohoho", "That's not true at all. Everything is wrong in this video...");
         Comment comment7 = new Comment("icecreammmy", "Hotpot Tonight!");
+
+        videoList.Add(video2);
+        video2.SetComment(comment5);
+        video2.SetComment(comment6);
+        video2.SetComment(comment7);
 
         Video video3 = new Video("The Fastest Car in the World", "JoJoBA", 600);
         Comment comment8 = new Comment("karenisthebest", "Come on...");
@@ -24,19 +35,7 @@ class Program
         Comment comment11 = new Comment("nononoxxxx", "ummmmmm....");
         Comment comment12 = new Comment("over_over", "boring");
 
-        videoList.Add(video1);
-        videoList.Add(video2);
         videoList.Add(video3);
-
-        video1.SetComment(comment1);
-        video1.SetComment(comment2);
-        video1.SetComment(comment3);
-        video1.SetComment(comment4);
-
-        video2.SetComment(comment5);
-        video2.SetComment(comment6);
-        video2.SetComment(comment7);
-
         video3.SetComment(comment8);
         video3.SetComment(comment9);
         video3.SetComment(comment10);
